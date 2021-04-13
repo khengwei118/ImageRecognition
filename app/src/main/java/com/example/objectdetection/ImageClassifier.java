bdb0d8986808c1600d5d1727e643aaea6a5f6aad
Kheng Wei Ang <khengwei118@gmail.com>
2021-04-13 00:59:50 -0700

    added sort to the predictions, fixed ImageClassifier class

--- app/src/main/java/com/example/objectdetection/ImageClassifier.java
+++ app/src/main/java/com/example/objectdetection/ImageClassifier.java
@@ -9,22 +9,35 @@ import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.support.common.FileUtil;
 import org.tensorflow.lite.support.common.TensorProcessor;
 import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+import org.tensorflow.lite.support.image.ops.Rot90Op;
+import org.tensorflow.lite.support.label.TensorLabel;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class ImageClassifier {
 
     private static final float PROBABILITY_MEAN = 0.0f;
     private static final float PROBABILITY_STD = 255.0f;
+    private static final float IMAGE_STD = 1.0f;
+    private static final float IMAGE_MEAN = 0.0f;
+    private static final int MAX_SIZE = 5;
+
+
     private final Interpreter tensorClassifier;
+    private final List<String> labels;
     private final int imageResizeX;
     private final int imageResizeY;
-    private final TensorImage inputImageBuffer;
+    private TensorImage inputImageBuffer;
     private final TensorBuffer probabilityImageBuffer;
     private final TensorProcessor probabilityProcessor;
 
@@ -32,7 +45,7 @@ public class ImageClassifier {
         //loading the model
         @NonNull MappedByteBuffer classifierModel = FileUtil.loadMappedFile(activity,
                 "mobilenet_v2_1.0_224_quant.tflite");
-        @NonNull List<String> labels = FileUtil.loadLabels(activity, "labels.txt");
+        labels = FileUtil.loadLabels(activity, "labels.txt");
 
         tensorClassifier = new Interpreter(classifierModel, null);
 
@@ -61,7 +74,31 @@ public class ImageClassifier {
     public List<Recognition> recognizeImage(final Bitmap bitmap, final int sensorOrientation) {
         List<Recognition> recognitions = new ArrayList<>();
         inputImageBuffer = loadImage(bitmap, sensorOrientation);
+        tensorClassifier.run(inputImageBuffer.getBuffer(), probabilityImageBuffer.getBuffer().rewind());
+        Map<String, Float> labelledProbability = new TensorLabel(labels,
+                probabilityProcessor.process(probabilityImageBuffer)).getMapWithFloatValue();
+        for (Map.Entry<String, Float> entry : labelledProbability.entrySet()) {
+            recognitions.add(new Recognition(entry.getKey(), entry.getValue()));
+        }
+        // sorting predictions based on confidence
+        Collections.sort(recognitions);
+        // returning top 5 predictions
+        recognitions.subList(0, MAX_SIZE > recognitions.size() ? recognitions.size() : MAX_SIZE).clear();
         return recognitions;
+    }
+
+    private TensorImage loadImage(Bitmap bitmap, int sensorOrientation) {
+        inputImageBuffer.load(bitmap);
+        int noOfRotations = sensorOrientation / 90;
+        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        ImageProcessor imageProcessor = new ImageProcessor.Builder()
+                .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                .add(new ResizeOp(imageResizeX, imageResizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                .add(new Rot90Op(noOfRotations))
+                .add(new NormalizeOp(IMAGE_MEAN, IMAGE_STD))
+                .build();
+        return imageProcessor.process(inputImageBuffer);
+
     }
 
     class Recognition implements Comparable {
